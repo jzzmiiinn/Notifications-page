f27440d65f16b72dfbb979f1e0cb17599feaa25c
jzzmiiinn <jzzmiiinnali@gmail.com>
2026-09-12 22:12:16 +0300

    Building using React

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
-import notifications from "./data/notifications";
-
-const [notifications, setNotifications] =
-  useState<Notification[]>(initialNotifications);
-
-function markAsRead() {
-  setNotifications(
-    notifications.map((notification) => ({
-      ...notification,
-      read: true,
-    })),
-  );
-}
-
-const unreadCount = notifications.filter(
-  (notification) => !notification.read,
-).length;
+import NotificationList from "./components/NotificationList";
+import { notifications as initialNotifications } from "./data/notifications";
+import type { Notification } from "./types/notification";
 
 function App() {
+  const [notifications, setNotifications] =
+    useState<Notification[]>(initialNotifications);
+
+  function markAsRead() {
+    setNotifications(
+      notifications.map((notification) => ({
+        ...notification,
+        read: true,
+      })),
+    );
+  }
+
+  const unreadCount = notifications.filter(
+    (notification) => !notification.read,
+  ).length;
+
   return (
-    <>
-      <div
-        id="app"
-        class="max-w-[700px] mx-auto bg-white min-h-screen p-6 mt-15"
-      >
-        <Header unreadCount={unreadCount} onMarkAsRead={markAsRead} />
-      </div>
-    </>
+    <div
+      id="app"
+      className="max-w-[700px] mx-auto bg-white min-h-screen p-6 mt-15"
+    >
+      <Header unreadCount={unreadCount} onMarkAsRead={markAsRead} />
+
+      <NotificationList notifications={notifications} />
+    </div>
   );
 }
+
+export default App;
--- src/components/NotificationList.tsx
+++ src/components/NotificationList.tsx
@@ -1,11 +1,18 @@
 import NotificationItem from "./NotificationItem";
+import type { Notification } from "../types/notification";
 
-function NotificationList() {
+interface NotificationListProps {
+  notifications: Notification[];
+}
+
+function NotificationList({ notifications }: NotificationListProps) {
   return (
     <div>
       {notifications.map((notification) => (
-        <NotificationItem notification={notification} />
+        <NotificationItem key={notification.id} notification={notification} />
       ))}
     </div>
   );
 }
+
+export default NotificationList;
